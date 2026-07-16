46e8ee23b033ad0a3072ddcc91f976e5fe1d01d9
samres <yososyam4@gmail.com>
2026-07-16 07:24:32 -0400

    new version gitignore

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('editar', views.editor, name='editar'),
+    #vistas general
     path('',views.solicitudes, name="solicitudes"),
+    path('borrar/<int:peticion>',views.solicitudes, name="borrar_peticion_uno"),
+    path('borrar/todo',views.solicitudes, name="borrar_peticion_todo"),
+    
+    #vistas editar
+    path('editar', views.editor, name='editar'),
+    
+    #vistas ejecutar
     path('respuestas',views.respuestas, name="respuestas")
 ]
